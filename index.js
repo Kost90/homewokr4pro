4e616a5fc4d925c753b035c71c4d1f2eb71808bb
kost90 <kostdonc90@gmail.com>
2023-02-28 12:53:15 +0000

    creat index.js

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const userMetod = prompt('What you want to do: add, sub, mult, div ?');
 const firstNumber = +prompt('What is first number?');
 const secondNumber = +prompt('What is second number?');
 if (userMetod === 'add'){
-    alert(firstNumber + secondNumber)
+    alert(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`)
 }
 if (userMetod === 'sub'){
-    alert(firstNumber - secondNumber)
+    alert(`${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}`)
 }
 if (userMetod === 'mult'){
-    alert(firstNumber * secondNumber)
+    alert(`${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}`)
 }
 if (userMetod === 'div'){
-    alert(firstNumber / secondNumber)
+    alert(`${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`)
 }
 
